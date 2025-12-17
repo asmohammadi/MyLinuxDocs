aabb999ed7fea71fb28ee45723c42ebf049231fd
Asghar Mohammadi <56192199+asmohammadi@users.noreply.github.com>
2025-12-17 15:07:40 +0330

    Adding Python code-13(Type)

---
+++ Python/code-13(Type).py
@@ -0,0 +1,13 @@
+my_int = 23
+my_float = 25.5
+my_string = 'hello'
+my_list = [1, 2, 'hello']
+my_dict = {'key1': 'value1'}
+my_tuple = (1 ,2, 'hello')
+my_boolean = True
+print(type(my_int)) # Get the Type of Variables
+print(type(my_list))
+print(type(my_dict))
+print(type(my_tuple))
+print(type(my_boolean))
+
